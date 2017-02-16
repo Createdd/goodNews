4635d60edb2dc11428405ddf150ad06f7d4dc77d
Daniel Deutsch <deudan1010@gmail.com>
2017-02-16 16:54:58 +0100

    add styling in list

--- src/components/List.js
+++ src/components/List.js
@@ -3,9 +3,16 @@ import React from 'react';
 export default class List extends React.Component {
   render() {
     return (
-      <ol>
-        {this.props.articles.map((item, key) => <li key={key}><b>{item.title}</b><br/>{item.author}{console.log(item)}</li>)}
-      </ol>
+      <div>
+        <h4>List of articles:</h4>
+        <ol>
+          {this.props.articles.map((item, key) => (
+            <li className="hoverable card-panel" key={key}>
+              <b>{item.title}</b><br />{item.author}<br />
+            </li>
+          ))}
+        </ol>
+      </div>
     );
   }
 }
