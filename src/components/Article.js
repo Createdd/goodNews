cab5d1368ced300610796196f6dedf4ba490f3c8
Daniel Deutsch <deudan1010@gmail.com>
2017-02-27 21:29:40 +0100

    add context and push to article route

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,17 +5,22 @@ export default class Article extends React.Component {
     super(props);
     this.displayArticle = this.displayArticle.bind(this);
   }
-  displayArticle() {
+  displayArticle(e) {
     let index = this.props.index +1;
-    console.info(index);
+    console.info(`/view/${index}`);
+    this.context.router.push(`/view/${index}`);
   }
   render() {
     return (
-      <div onClick={this.displayArticle}>
+      <div onClick={e => this.displayArticle(e)}>
         {this.props.index +1}<br/>
         <b>{this.props.article.title}</b><br/>
         {this.props.article.author}<br/>
       </div>
     );
   }
+}
+
+Article.contextTypes = {
+  router: React.PropTypes.object
 }
