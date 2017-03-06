01b87d434a0ab3cbeed0fb0b265dadec3e45301e
Daniel Deutsch <deudan1010@gmail.com>
2017-03-06 12:38:28 +0100

    prettify ArticleDetail

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -6,7 +6,7 @@ export default class ArticleDetail extends React.Component {
       <div>
         <p>This is article number: {this.props.params.articleId}</p>
         {Object.keys(this.props.state.showArticle).map(key => (
-          <p key={key}>{this.props.state.showArticle[key]}{console.info(this.props.state.showArticle)}</p>
+          <p key={key}>{this.props.state.showArticle[key]}</p>
 
         ))}
       </div>
